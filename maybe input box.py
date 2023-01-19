c3279535b15cdb8bd177aa678aa5b44f927b1308
Joe Cherveny <108467883+DaddyChill4205@users.noreply.github.com>
2023-01-19 11:30:36 -0500

    added collor to all boxes

--- maybe input box.py
+++ maybe input box.py
@@ -1,49 +1,11 @@
 import tkinter as tk
 from tkinter import *    
 
-class DoubleInputBox(object):
-    def __init__(self, text1, text2, title = ''):
-        self.value = None
-        self.root = None
-        self.text1 = text1
-        self.text2 = text2
-        self.title = title
-        self.result = None
+gui = Tk(className='Python Examples - Window Color')
+# set window size
+gui.geometry("400x200")
 
-    def center(self):
-        self.root.update_idletasks()
-        width, height = (self.root.winfo_width(), self.root.winfo_height())
-        x = (self.root.winfo_screenwidth() // 2) - (width // 2)
-        y = (self.root.winfo_screenheight() // 2) - (height // 2) - 50 # 50 is for the awkwardness of the taskbar
-        self.root.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+#set window color
+gui['background']='#856ff8'
 
-    def return_entry(self):
-        self.result = self.entry1.get()
-        self.result2 = self.entry2.get()
-        self.root.destroy()
-
-    def input(self):
-        self.root = tk.Tk()
-        self.root.attributes("-topmost", True)
-        self.root.focus_force() # just in case
-        self.root.title(self.title)
-        Label(self.root, text=self.text1).grid(columnspan = 2)
-        self.entry1 = Entry(self.root)
-        self.entry1.focus_set()
-        self.entry1.grid(columnspan = 2)
-        Label(self.root, text=self.text2).grid(columnspan = 2)
-        self.entry2 = Entry(self.root)
-        self.entry2.grid(columnspan = 2)
-        self.ok_button = Button(self.root, text = "OK", command = self.return_entry)
-        self.ok_button.grid(row=4, column=0, padx=25, pady=10)
-        Button(self.root, text = "Cancel", command = lambda: self.root.destroy()).grid(row=4, column=1, padx=25, pady=10)
-        self.center()
-        self.root.bind("<Return>", lambda e = None: self.return_entry())
-        self.root.bind("<Escape>", lambda e = None: self.root.destroy())
-        self.root.mainloop()
-
-box = DoubleInputBox("Enter your name", "Name")
-box.input()
-result = box.result
-result2 = box.result2
-print("result:", result, result2)
+gui.mainloop()
--- message_boxes.py
+++ message_boxes.py
@@ -7,12 +7,15 @@ class ButtonBox(object):
     A message box that returns the text of the button pressed. Can have as many buttons as you want, as long as there is at least one. 
     
     '''
-    def __init__(self, text, title, button_options):
+    def __init__(self, text, title, button_options, Wbg="white", Lbg="white", Lfg="black"):
         self.value = None
         self.root = None
         self.button_options = button_options
         self.text = text
         self.title = title
+        self.Wbg = Wbg
+        self.Lbg = Lbg
+        self.Lfg = Lfg
     def center(self):
         self.root.update_idletasks()
         width, height = (self.root.winfo_width(), self.root.winfo_height())
@@ -21,10 +24,11 @@ class ButtonBox(object):
         self.root.geometry('{}x{}+{}+{}'.format(width, height, x, y))
     def options(self):
         self.root = tk.Tk()
+        self.root['background'] = self.Wbg
         self.root.attributes("-topmost", True)
         self.root.focus_force() # just in case
         self.root.title(self.title)
-        Label(self.root, text=self.text).grid(columnspan=len(self.button_options), padx=25, pady=10)
+        Label(self.root, text=self.text, bg=self.Lbg, fg=self.Lfg).grid(columnspan=len(self.button_options), padx=25, pady=10)
         if type(self.button_options) == str:
             self.button_options = [self.button_options]
         if type(self.button_options) == list or tuple:
@@ -47,13 +51,17 @@ class InputBox(object):
     A message box that returns the text of the entry.
     
     '''
-    def __init__(self, text, title = '', show = None):
+    def __init__(self, text, title = '', show = None, Wbg="white", Lbg="white", Ebg="white", Lfg="black"):
         self.value = None
         self.root = None
         self.text = text
         self.title = title
         self.show = show
         self.result = None
+        self.Lbg = Lbg
+        self.Wbg = Wbg
+        self.Ebg = Ebg
+        self.Lfg = Lfg
 
     def center(self):
         self.root.update_idletasks()
@@ -68,11 +76,12 @@ class InputBox(object):
 
     def input(self):
         self.root = tk.Tk()
+        self.root['background'] = self.Wbg
         self.root.attributes("-topmost", True)
         self.root.focus_force() # just in case
         self.root.title(self.title)
-        Label(self.root, text=self.text).grid(columnspan = 2)
-        self.entry = Entry(self.root, show=self.show)
+        Label(self.root, text=self.text, bg=self.Lbg, fg=self.Lfg).grid(columnspan = 2)
+        self.entry = Entry(self.root, show=self.show, bg=self.Ebg)
         self.entry.focus_set()
         self.entry.grid(columnspan = 2)
         self.ok_button = Button(self.root, text = "OK", command = self.return_entry)
@@ -88,7 +97,7 @@ class DoubleInputBox(object):
     A message box that allows for two seperate text entries, with two different labels. Wil return ("text_input1", "text_input2").
     
     '''
-    def __init__(self, text1, text2, title = '', show1 = None, show2 = None):
+    def __init__(self, text1, text2, title = '', show1 = None, show2 = "*", Wbg="white", Lbg1="white", Lfg1="black", Lbg2="white", Lfg2="black", Ebg1="white", Ebg2="white"):
         self.value = None
         self.root = None
         self.text1 = text1
@@ -97,6 +106,13 @@ class DoubleInputBox(object):
         self.show1 = show1
         self.show2 = show2
         self.result = None
+        self.Lbg1 = Lbg1
+        self.Lbg2 = Lbg2
+        self.Wbg = Wbg
+        self.Ebg1 = Ebg1
+        self.Ebg2 = Ebg2
+        self.Lfg1 = Lfg1
+        self.Lfg2 = Lfg2
 
     def center(self):
         self.root.update_idletasks()
@@ -112,15 +128,16 @@ class DoubleInputBox(object):
 
     def input(self):
         self.root = tk.Tk()
+        self.root['background'] = self.Wbg
         self.root.attributes("-topmost", True)
         self.root.focus_force() # just in case
         self.root.title(self.title)
-        Label(self.root, text=self.text1).grid(columnspan = 2)
-        self.entry1 = Entry(self.root, show=self.show1)
+        Label(self.root, text=self.text1, bg=self.Lbg1, fg=self.Lfg1).grid(columnspan = 2)
+        self.entry1 = Entry(self.root, show=self.show1, bg=self.Ebg1)
         self.entry1.focus_set()
         self.entry1.grid(columnspan = 2)
-        Label(self.root, text=self.text2).grid(columnspan = 2)
-        self.entry2 = Entry(self.root, show=self.show2)
+        Label(self.root, text=self.text2, bg=self.Lbg2, fg=self.Lbg2).grid(columnspan = 2)
+        self.entry2 = Entry(self.root, show=self.show2, bg=self.Ebg2)
         self.entry2.grid(columnspan = 2)
         self.ok_button = Button(self.root, text = "OK", command = self.return_entry)
         self.ok_button.grid(row=4, column=0, padx=25, pady=10)
@@ -143,17 +160,27 @@ def input_box(text=None, title=None, show=None):
 def double_input_box(text1=None, text2=None, title=None, show1=None, show2=None):
     box = DoubleInputBox(text1=text1, text2=text2, title=title, show1=show1, show2=show2)
     box.input()
-    result = box.result
-    result2 = box.result2
-    final_result = result, result2
+    try:
+        final_result = box.result, box.result2
+    except AttributeError as e:
+        print(e)
+        final_result = None
     return final_result
 
 def login():
-    r = double_input_box("Username:", "Password:", "Login", show2="*")
-    if r == ("Joe", "123"):
-        button_box("Login successful!", "Login")
-    else:
+    r = double_input_box("Username:", "Password:", "Login")
+    if r == None:
+        print("Login cancelled.")
+    if r != ("Joe", "123") and r != None:
         button_box("Login failed!", "Login")
         login()
+    if r == ("Joe", "123"):
+        r = button_box("Login successful!", "Login")
+        if r == "Ok":
+            r = input_box("What would you like to do today?", "Login")
+            if r == "Sleep":
+                r = button_box("Same lol", "Login")          
 
-login()
+
+
+input_box("Hello!")
